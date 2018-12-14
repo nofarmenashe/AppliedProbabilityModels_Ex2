03850fbe55c5855bb42f458d96b5ede7ed210357
Unknown <nofar1407@gmail.com>
2018-12-14 18:46:36 +0200

    Auto stash before merge of "master" and "origin/master"

--- ex2.py
+++ ex2.py
@@ -27,9 +27,15 @@ def get_all_words_in_articles(articles):
     return words
 
 
-def number_of_different_events(dataset):
-    distinct_dataset = list(set(dataset))
-    return len(distinct_dataset)
+def check_sum_of_probabilities(model_func, lambda_param, train_set_collection, train_set_size):
+    n0 = VOCABULARY_SIZE - len(training_set_collection)
+    sum_of_probabilities = n0 * model_func(lambda_param, UNSEEN_WORD, train_set_collection, training_set_size)
+
+    for word in train_set_collection.keys():
+        sum_of_probabilities += model_func(lambda_param, word, train_set_collection, train_set_size)
+
+    print(model_func, sum_of_probabilities)
+
 
 def MLE(event, dataset_collection, dataset_size):
     event_count = dataset_collection[event]
@@ -58,7 +64,6 @@ def get_lambda_with_min_perplexity(test_set, train_set_collection, train_set_siz
     min_lambda = None
 
     for lambda_param in lambda_array:
-        print(lambda_param)
         curr_perplexity = perplexity(lambda_param, test_set, train_set_collection, train_set_size)
 
         if curr_perplexity < min_perplexity:
@@ -100,6 +105,9 @@ if __name__ == "__main__":
     developmentArticles = get_articles_from_file(development_set_filename)
     development_set_words = get_all_words_in_articles(developmentArticles)
 
+    testArticles = get_articles_from_file(test_set_filename)
+    test_set_words = get_all_words_in_articles(testArticles)
+
     outputs = [None for _ in range(29)]
 
     # 1. Init
@@ -138,9 +146,9 @@ if __name__ == "__main__":
     outputs[17] = perplexity(0.1, validation_set, training_set_collection, len(training_set))
     outputs[18] = perplexity(1, validation_set, training_set_collection, len(training_set))
 
-    outputs[19], outputs[20] = get_lambda_with_min_perplexity(validation_set, training_set_collection, len(training_set))
-    # 4. Held out model training
-    held_out_training_set_size = round(0.5 * len(development_set_words))
+    best_lambda, min_perplexity = get_lambda_with_min_perplexity(validation_set, training_set_collection, len(training_set))
+    outputs[19] = best_lambda
+    outputs[20] = min_perplexity
 
     S_t = development_set_words[:held_out_training_set_size]
     S_t_collection = collections.Counter(S_t)
@@ -154,6 +162,21 @@ if __name__ == "__main__":
     outputs[23] = held_out_model(INPUT_WORD, S_t_collection, S_h_collection, len(S_h), VOCABULARY_SIZE)
     outputs[24] = held_out_model(UNSEEN_WORD, S_t_collection, S_h_collection, len(S_h), VOCABULARY_SIZE)
 
+
+    # 5. debug models
+    check_sum_of_probabilities(lidstone_unigram_model, 0.1, training_set_collection, len(training_set))
+    # TODo: add heldout model
+
+    # 6.
+    outputs[25] = len(test_set_words)
+
+    lidstone_perplexity = perplexity(best_lambda, test_set_words, training_set_collection, len(training_set))
+    outputs[26] = lidstone_perplexity
+
+    heldout_perplexity = 0.0 # Todo: call heldout perplexity function
+    outputs[27] = heldout_perplexity
+
+    outputs[28] = 'L' if lidstone_perplexity > heldout_perplexity else 'H'
 
     # Write final output
     output_string = "#Student\tYuval Maymon\tNofar Menashe\t315806299\t 205486210\n"
