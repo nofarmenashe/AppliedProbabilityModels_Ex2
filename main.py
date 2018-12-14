b8873d31fad766337416c97c3aa62db74d1cdefe
nofarMenashe <nofar1407@gmail.com>
2018-12-14 11:51:22 +0200

    add main func and args

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == "__main__":
+    print("start program")
+
+    dev_set_filename = sys.argv[1]
+    test_set_filename = sys.argv[2]
+    input_word = sys.argv[3]
+    output_filename = sys.argv[4]
