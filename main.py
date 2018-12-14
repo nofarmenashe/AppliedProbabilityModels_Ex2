d4f1e365ddfc2eae59a65261d0ee27ea1b44d3f8
Unknown <nofar1407@gmail.com>
2018-12-14 14:16:20 +0200

    outputs 8-11

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import sys
 
+
+def number_of_different_events(dataset):
+    distinct_dataset = list(set(dataset))
+    return len(distinct_dataset)
+
+
+def number_of_times_event_appear(event, dataset):
+    return dataset.count(event)
+
+
 if __name__ == "__main__":
 
     development_set_filename = sys.argv[1]
@@ -22,6 +32,19 @@ if __name__ == "__main__":
     outputs[6] = P_uniform
 
     # 2. Development set preprocessing
+
+
+    # 3. Lindstone model training
+    development_set = [None for _ in range(29)] # change to real development set
+    training_set_size = round(0.9 * len(development_set))
+
+    training_set = development_set[:training_set_size]
+    validation_set = development_set[training_set_size:]
+
+    outputs[8] = len(validation_set)
+    outputs[9] = len(training_set)
+    outputs[10] = number_of_different_events(training_set)
+    outputs[11] = number_of_times_event_appear(INPUT_WORD, training_set)
 
     # Write final output
     outputString = "#Student\tYuval Maymon\tNofar Menashe\t315806299\t 205486210\n"
