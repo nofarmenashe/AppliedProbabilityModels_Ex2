3ca65ad5722353365bb3021581ca07970fc237e2
maymonyu <yuvalmaymon30@gmail.com>
2018-12-14 12:14:16 +0200

    yuva's first commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
 if __name__ == "__main__":
-    print("start program")
 
     dev_set_filename = sys.argv[1]
     test_set_filename = sys.argv[2]
     input_word = sys.argv[3]
     output_filename = sys.argv[4]
+
+    print(dev_set_filename, test_set_filename, input_word, output_filename)
